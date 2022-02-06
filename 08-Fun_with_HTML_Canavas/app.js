e95f9719decb1bd5827ea7168798956d8bece21f
Darshan M Hegde <hegdes123@gmail.com>
2022-02-06 20:09:57 +0530

    08 new update

--- 08-Fun_with_HTML_Canavas/app.js
+++ 08-Fun_with_HTML_Canavas/app.js
@@ -52,7 +52,11 @@ canvas.addEventListener("mousemove", draw);
 canvas.addEventListener("mouseup", () => (isDrawing = false));
 canvas.addEventListener("mouseout", () => (isDrawing = false));
 
-canvas.addEventListener("touchstart", draw);
+canvas.addEventListener("touchstart", (e) => {
+  isDrawing = true;
+  [lastX, lastY] = [e.offsetX, e.offsetY];
+});
+
+canvas.addEventListener("touchmove", draw);
 canvas.addEventListener("touchend", () => (isDrawing = false));
 canvas.addEventListener("touchcancel", () => (isDrawing = false));
-canvas.addEventListener("touchmove", draw);
